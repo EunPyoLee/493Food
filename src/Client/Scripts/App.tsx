658138d4f39de5981755da44bec3a76d4764271a
eunpyo <eunpyo@umich.edu>
2020-11-16 01:10:15 -0500

    Router set-up for Home, Recipes, Recipe, Register

--- src/Client/Scripts/App.tsx
+++ src/Client/Scripts/App.tsx
@@ -1,11 +1,26 @@
 import * as React from 'react';
+import {Switch, BrowserRouter, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      Main App
-    </div>
-  );
+import Constants from './Constants';
+
+import Home from './Components/Home';
+import Recipes from './Components/Recipes';
+import Recipe from './Components/Recipe';
+import Register from './Components/Register';
+
+const App: React.FC<{}> = () => {
+  const {routes} = Constants;
+  const {home, recipes, recipe, register} = routes;
+  return(
+    <BrowserRouter>
+         <Switch>
+            <Route exact path={home} component={Home} />
+            <Route exact path={recipes} component={Recipes} />
+            <Route exact path={recipe} component={Recipe} />
+            <Route exact path={register} component={Register} />
+         </Switch>
+      </BrowserRouter>
+  )
 }
 
-export default App;
+export default React.memo(App);
--- src/Client/Scripts/Constants.ts
+++ src/Client/Scripts/Constants.ts
@@ -1,5 +1,10 @@
 //Define constants here
 const constants = {
-
+    routes: {
+        home: '/',
+        recipes: '/recipes',
+        recipe: '/recipe',
+        register: '/register'
+    }
 };
 export default Object.freeze(constants);
