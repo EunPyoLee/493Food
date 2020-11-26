134a100182be5ceea83169c8e4a6c26c63405ae5
christa hansma <christahansma@christas-MacBook-Pro.local>
2020-11-26 10:59:13 -0500

    upload recipe almost done, need to fix image routing

---
+++ src/Client/Scripts/Models/INewRecipeData.ts
@@ -0,0 +1,28 @@
+import {Ingredient} from './IRecipeData'
+
+export interface NewRecipeData {
+    ingredients: Ingredient[];
+    steps: string[];
+}
+
+export interface NewRecipeInput {
+   nameInput: string;
+   timeInput: number;
+   unitInput: string;
+   typeInput: string;
+   imgInput: string;
+   ingredientInput: string;
+   quantityInput: string;
+   stepInput:string;
+}
+
+export interface RecipeToAdd {
+  recipeId: number;
+  name: string;
+  imgLink: string;
+  time: number;
+  unit: string;
+  type: string;
+  ingredients: Ingredient[];
+  steps: string[];
+}
