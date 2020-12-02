4af7227d290ee399a478ec2d21a42313df6791a2
keara <kearabird@yahoo.com>
2020-12-01 18:03:28 -0800

    Grocery added, issue in the toggle causing compile issue

--- src/Client/Scripts/Components/Groceries.tsx
+++ src/Client/Scripts/Components/Groceries.tsx
@@ -1,6 +1,15 @@
 import * as React from 'react';
 import {Link} from 'react-router-dom';
+import { IGroceryList, list } from '../Models/IGroceryList';
+
+
+
 const Groceries: React.FC<{}> = () => {
+
+   let empty = <p>Your list is currently empty!</p>
+   if(list[0]){
+      empty = <p></p>
+   };
    return (
       <div className="register-wrapper">
          <Link to="/">
@@ -10,9 +19,12 @@ const Groceries: React.FC<{}> = () => {
          </Link>
          <h1>My Grocery List</h1>
          <ul className="grocery-list">
-            <li>Brownie Mix<button className="delete-button">x</button></li>
-            <li>Eggs<button className="delete-button">x</button></li>
-            <li>Oil<button className="delete-button">x</button></li>
+            {empty}
+            {list?.map(item => (
+               <li>{item?.quantity} {' of '} {item?.ingredientName}<button ref={item?.ingredientName} className="delete-button" onClick="(function(item){
+                  list.splice(list.findIndex(item.ingredientName,1)
+              })();return false;">x</button></li>
+            ))}
          </ul>
          <div className="send-button-wrapper">
             <button className="send-button">Send me my list!</button>
